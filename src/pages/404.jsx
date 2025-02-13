21d56e6f302d95d04f92764382458bf4c4283331
Nemrik <aymericmorel55@gmail.com>
2025-02-13 14:24:39 +0100

    404 maj

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -9,13 +9,13 @@ export default function NotFoundPage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center px-6">
-      <h1 className="text-6xl font-bold text-white">404</h1>
-      <p className="text-xl text-gray-300 mt-4">
+      <h1 className="text-8xl font-bold text-white">404</h1>
+      <p className="text-2xl text-gray-300 mt-6">
         La page que vous cherchez a expiré ou n&apos;existe plus.
       </p>
       <button
         onClick={handleHomePageRedirect}
-        className="mt-6 px-4 py-2 bg-orange-600 text-white rounded-lg shadow-lg"
+        className="mt-8 px-6 py-3 bg-[#FF8C00] hover:bg-orange-600 hover:scale-102 transition-all duration-500 text-white rounded-lg shadow-lg"
       >
         Retour à l&apos;accueil
       </button>
